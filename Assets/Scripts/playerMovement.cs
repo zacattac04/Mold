1517fc4d64ecc96f153f84fe12999dc56417f312
Joey Kihn <jokihn2002@gmail.com>
2024-05-17 15:39:22 -0400

    reverting air walk glitch
    made other collisions, much more glitchy

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -206,7 +206,7 @@ public class playerMovement : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision){
         if(collision.gameObject.CompareTag(GROUND_TAG)){
             onGround = true;
-            anim.SetBool("IsFalling", false);
+            //anim.SetBool("IsFalling", false);
             if(anim.GetBool("IsJumping")){
                 anim.SetBool("IsJumping", false);
             }
@@ -220,7 +220,7 @@ public class playerMovement : MonoBehaviour
             //if player is on top of the box, allow them to jump, but no push animation
             if(collision.gameObject.transform.position.y < transform.position.y){
                 onGround = true;
-                anim.SetBool("IsFalling", false);
+                //anim.SetBool("IsFalling", false);
                 if(anim.GetBool("IsJumping")){
                     anim.SetBool("IsJumping", false);
                 }
@@ -241,9 +241,10 @@ public class playerMovement : MonoBehaviour
         if(collision.gameObject.CompareTag(BLOCK_TAG)){
             anim.SetBool("IsPushing", false);
         }
+        /*
         else if(collision.gameObject.CompareTag(GROUND_TAG)){
             anim.SetBool("IsFalling", true);
-        }
+        }*/
     }
 
     private void OnTriggerEnter2D(Collider2D col) {
